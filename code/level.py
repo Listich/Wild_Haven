65a55b54b918bda266a444cb642ea475db3d1c11
serenachristy <serena.kifoula@epitech.eu>
2024-05-17 00:04:57 +0200

    ADD: function call process_layer this tool allows me to build my character's house

--- code/level.py
+++ code/level.py
@@ -4,6 +4,7 @@ from settings import *
 from player import Player
 from overlay import Overlay
 from sprites import Generic
+from pytmx.util_pygame import load_pygame
 
 
 class Level:
@@ -15,10 +16,27 @@ class Level:
 		# sprite groups
 		self.all_sprites = CameraGroup()
 
+		#telecharge l'image de la maison
+		self.tmx_data = load_pygame('../data/map.tmx')
+
+		#layer constants
+		self.LAYERS_HOUSE_BOTTOM = ['HouseFloor', 'HouseFurnitureBottom']
+		self.LAYERS_MAIN = ['HouseWalls', 'HouseFurnitureTop']
+
 		self.setup()
 		self.overlay = Overlay(self.player)
 
+
+#process_layer cette fonction me permet de construire la maison
+	def process_layer(self, layer_names, layer_group, tile_size, all_sprites):
+		for layer in layer_names:
+			for x, y, surf in self.tmx_data.get_layer_by_name(layer).tiles():
+				Generic((x * tile_size, y * tile_size), surf, all_sprites, layer_group)
 	def setup(self):
+		self.process_layer(self.LAYERS_HOUSE_BOTTOM, LAYERS['house bottom'], TILE_SIZE, self.all_sprites)
+		self.process_layer(self.LAYERS_MAIN, LAYERS['main'], TILE_SIZE, self.all_sprites)
+
+		#Fence
 		self.player = Player((640,360), self.all_sprites)
 		path_floor = os.path.join('..', 'graphics', 'world', 'ground.png')
 		Generic(
